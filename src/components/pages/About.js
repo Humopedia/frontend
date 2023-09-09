ffd387333bfa42033c11344a43c18bf9a03b51fe
gajapathi22 <gajapathikikkara@gmail.com>
2023-09-09 12:21:57 +0530

    commited for backend integration

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -23,24 +23,24 @@ const About = () =>{
                      </div>
                 </div>
               </div>
-              <diV className="flex flex-row p-10 pb-12 border border-8 border-b-0 border-indigo-300">
+              <diV className="flex flex-row p-10 pb-12 border border-8 border-b-0 border-indigo-300 ">
     
                  
                     <div className="  basis-1/3">
                           <img className="h-full transform scale-90 w-full" src={image4}/>
                     </div>
                     
-                      <div className=" basis-2/3 flex flex-col items-start gap-1 justify-center">
+                      <div className=" basis-2/3 flex flex-col items-start gap-1 justify-center ">
                         <div>
                             <h1 className="text-7xl text-stroke-2 font-mono font-bold ">Our Mission</h1>
                         </div>
                         <div >
-                            <h1 className="text-2xl   ">At Humopedia, our mission is to create an innovative digital space where the intricate tapestries of individual lives are woven together, creating a mosaic of shared wisdom and insight. We are dedicated to fostering understanding, promoting cross-cultural interactions, and enriching the lives of both storytellers and readers.</h1>
+                            <h1 className="text-2xl ">At Humopedia, our mission is to create an innovative digital space where the intricate tapestries of individual lives are woven together, creating a mosaic of shared wisdom and insight. We are dedicated to fostering understanding, promoting cross-cultural interactions, and enriching the lives of both storytellers and readers.</h1>
                         </div>
                       </div>
                       
               </diV>
-              <diV className="flex flex-row p-10 bg-white border border-8 border-b-0  border-indigo-300 ">
+              <diV className="flex flex-row p-10 border border-8 border-b-0  border-indigo-300 bg-slate-300 ">
                   <div className="flex justify-center items-center  basis-1/2">
                      <h1 className="text-4xl font-bold ">Vision and Values</h1>
                   </div>
@@ -61,11 +61,11 @@ const About = () =>{
                   
               </diV>
               <diV className=" flex flex-row ">
-                  <div className="flex flex-col basis-1/6  bg-white">
-                    <div className="basis-1/2 border border-b-4 border-l-8 border-indigo-300 "></div>
-                    <div className="basis-1/2 border border-t-4 border-l-8  border-indigo-300 "></div>
+                  <div className="flex flex-col basis-1/6 bg-slate-300">
+                    <div className="basis-1/2  border-b-4 border-l-8 border-indigo-300 "></div>
+                    <div className="basis-1/2  border-t-4 border-l-8  border-indigo-300 "></div>
                   </div>
-                  <div className="flex flex-col  bg-slate-400 border border-8 border-indigo-300 p-8 gap-4">
+                  <div className="flex flex-col  bg-slate-400 border border-8 border-indigo-300 drop-shadow-2xl p-8 gap-4">
                       <diV className=" flex justify-center">
                         <h1 className="text-4xl font-bold">Exploring Humopedia</h1>
                       </diV>
@@ -80,14 +80,14 @@ const About = () =>{
                         </ul>
                       </div>
                   </div> 
-                  <div className="flex flex-col bg-white  basis-1/6">
-                     <div className="basis-1/2  border border-b-4 border-r-8 border-indigo-300"></div>
-                     <div className="basis-1/2  border-t-4 border-r-8 border-indigo-300"></div>
+                  <div className="flex flex-col bg-slate-300   basis-1/6 ">
+                     <div className="basis-1/2   border-b-4 border-r-8 border-indigo-300"></div>
+                     <div className="basis-1/2    border-t-4 border-r-8 border-indigo-300"></div>
                   </div>  
               </diV>
-              <div className="flex flex-row bg-white">
+              <div className="flex flex-row border-x-8 border-b-8 border-indigo-300 bg-slate-300">
               
-                  <div className="  py-20 p-10 flex flex-row">
+                  <div className=" basis-2/3  py-20 p-10 flex flex-row">
                     <div className=" flex flex-col gap-4">
                         <div className="">
                           <h1 className="text-4xl font-bold">Join us</h1>
@@ -98,10 +98,10 @@ const About = () =>{
                     </div>
                   </div>         
                 
-                {/* <div className="drop-shawdow-lg basis-1/3  ">
+                <div className="drop-shawdow-lg basis-1/3  ">
                       <img  className=" pt-10 pr-10 " src={handshake}/>
                     
-                </div>  */}
+                </div> 
               </div>
           </div>
 
